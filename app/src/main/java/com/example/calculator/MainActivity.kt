b58a8b0ddf3d52b41782b41bb7e8403f9be79eb8
Daniel <yabomonkey@gmail.com>
2021-12-31 11:48:50 -0800

    Removed commented out items

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -23,33 +23,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        result = findViewById(R.id.result)
-//        newNumber = findViewById(R.id.newNumber)
-//
-//        // Data input buttons
-//        val button0: Button = findViewById(R.id.button0)
-//        val button1: Button = findViewById(R.id.button1)
-//        val button2: Button = findViewById(R.id.button2)
-//        val button3: Button = findViewById(R.id.button3)
-//        val button4: Button = findViewById(R.id.button4)
-//        val button5: Button = findViewById(R.id.button5)
-//        val button6: Button = findViewById(R.id.button6)
-//        val button7: Button = findViewById(R.id.button7)
-//        val button8: Button = findViewById(R.id.button8)
-//        val button9: Button = findViewById(R.id.button9)
-//        val buttonDot: Button = findViewById(R.id.buttonDot)
-//
-//        // Operation Buttons
-//        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
-//        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
-//        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
-//        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
-//        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
-
         val listener = View.OnClickListener { v ->
             val b = v as Button
             newNumber.append(b.text)
         }
+
+        var a = 1
 
         val dotListener = View.OnClickListener { v ->
             val b = v as Button
