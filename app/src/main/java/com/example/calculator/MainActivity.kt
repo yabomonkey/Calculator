2969327a8854da9c5303dad50ad299ccbfd969cb
Dan <yabomonkey@gmail.com>
2021-12-30 22:51:40 -0800

    Removed unneeded code that was previously commented out

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -11,9 +11,6 @@ private const val STATE_OPERAND1 = "Operand1"
 private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var result: EditText
-//    private lateinit var newNumber: EditText
-//    private val displayOperation by lazy(NONE) { findViewById<TextView>(R.id.operation) }
 
     //Variables to hold the operands and type of calculation
     private var operand1: Double? = null
@@ -22,29 +19,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        result = findViewById(R.id.result)
-//        newNumber = findViewById(R.id.newNumber)
-//
-//        // Data input buttons
-//        val button0: Button = findViewById(R.id.button0)
-//        val button1: Button = findViewById(R.id.button1)
-//        val button2: Button = findViewById(R.id.button2)
-//        val button3: Button = findViewById(R.id.button3)
-//        val button4: Button = findViewById(R.id.button4)
-//        val button5: Button = findViewById(R.id.button5)
-//        val button6: Button = findViewById(R.id.button6)
-//        val button7: Button = findViewById(R.id.button7)
-//        val button8: Button = findViewById(R.id.button8)
-//        val button9: Button = findViewById(R.id.button9)
-//        val buttonDot: Button = findViewById(R.id.buttonDot)
-//
-//        // Operation Buttons
-//        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
-//        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
-//        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
-//        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
-//        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
 
         val listener = View.OnClickListener { v ->
             val b = v as Button
